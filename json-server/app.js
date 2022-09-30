93b53b891b16f71b1b6baaf79778f88d035fad60
ShafiArnob <shafiarnob@gmail.com>
2022-09-30 13:54:57 +0600

    json-server post data

--- json-server/app.js
+++ json-server/app.js
@@ -7,3 +7,25 @@ fetch("http://localhost:3000/companies")
 .then(res => res.json())
 .then(data => console.log(data))
 
+const newData = {
+  email: "khan@gmail.com",
+  fname:"Khan"
+}
+
+// post 
+/*
+fetch('http://localhost:3000/users', {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(newData),
+  })
+  .then((response) => response.json())
+  .then((data) => {console.log('Success:', data);})
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+*/
+
+//delete
