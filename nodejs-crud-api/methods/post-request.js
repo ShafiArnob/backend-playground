2141ccf8ff3b0d949379212079a143d83707bd87
ShafiArnob <shafiarnob@gmail.com>
2022-10-15 20:01:00 +0600

    post data

--- nodejs-crud-api/methods/post-request.js
+++ nodejs-crud-api/methods/post-request.js
@@ -1,3 +1,25 @@
-module.exports = (req, res) =>{
-  
+const crypto = require('crypto')
+const requestBodyParser = require('../utils/body-parser')
+const writeToFile = require('../utils/write-to-file')
+
+module.exports = async (req, res) =>{
+  if(req.url === "/api/movies"){
+    try{
+      let body = await requestBodyParser(req) 
+      body.id = crypto.randomUUID()
+      // console.log(body);
+      req.movies.push(body)
+      writeToFile(req.movies)
+      res.writeHead(201,{"Content-Type":"application/json"})
+      res.end()
+    }
+    catch(err){
+      res.writeHead(400,{"Content-Type":"application/json"})
+      res.end(JSON.stringify({"title":"Validation Failed","message":"Request Body not Valid"}))
+    }
+  }
+  else{
+    res.writeHead(404,{"Content-Type":"application/json"})
+    res.end(JSON.stringify({"title":"Not Found","message":"Route Not Found"}))
+  }
 }
