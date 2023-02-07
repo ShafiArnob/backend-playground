dd2c0a7b1103cd5808905018c76d16ff54803d63
ShafiArnob <shafiarnob@gmail.com>
2023-02-07 17:38:05 +0600

    code refactor and create student controller

--- node-express-mongo-playground/mongo-express/app.js
+++ node-express-mongo-playground/mongo-express/app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const {insertStudent, searchStudent, getStudentById} = require("./student.service")
 const app = express()
 
 
@@ -7,31 +6,7 @@ const print = console.log
 
 app.use(express.json())
 
-app.get("/api/students/detail/:id", async(req, res)=>{
-  print("GET /api/students",req.params)
-  const student = await getStudentById(req.params.id)
-  res.send(student)
-})
-app.post("/api/students/search", async(req,res)=>{
-  print("POST /api/students/search",req.body)
-  const result = await searchStudent(req.body)
-  res.send(result)
-})
-app.post("/api/students/create", async(req, res)=>{
-  print("POST /api/students/create", req.body)
-  const result = await insertStudent(req.body)
-  res.send(result)
-})
 
-app.put("/api/students/update/:id", (req, res)=>{
-  print("PUT /api/students/:id", req.params.id)
-  res.send("Hello World")
-})
-
-app.delete("/api/students/delete/:id", (req, res)=>{
-  print("DELETE /api/students/:id", req.params.id)
-  res.send("Hello World")
-})
 
 
 app.use("/", (req,res)=>{
