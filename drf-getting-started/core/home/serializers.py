ba3d13e337270c6b6382d6ea96fdda988b4f741f
ShafiArnob <shafiarnob@gmail.com>
2023-06-13 11:14:34 +0600

    crud operation using serializers

--- drf-getting-started/core/home/serializers.py
+++ drf-getting-started/core/home/serializers.py
@@ -4,7 +4,7 @@ from .models import Person
 class PeopleSerializers(serializers.ModelSerializer):
   class Meta:
     model = Person
-    # fields you want to serialize
+    # fields you want to serialize or Required fields
     # if specific fields -> ["name", "age"]
     # if you want to add all the fields excluding some fields 
     # then exclude = ["age"]
--- drf-getting-started/core/home/views.py
+++ drf-getting-started/core/home/views.py
@@ -1,6 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from home.models import Person
+from home.serializers import PeopleSerializers
+
 @api_view(["GET", "POST", "PUT"]) # for http methods
 def index(request):
   courses = {
@@ -26,3 +29,58 @@ def index(request):
   elif request.method == 'PUT':
     print("PUT Method")
     return Response(courses)
+
+@api_view(['GET', 'POST', "PUT", "PATCH", "DELETE"])
+def person(request):
+  if request.method == "GET":
+    # get all the data from DB
+    objs = Person.objects.all()
+    # convert them to JSON
+    # many=True bc there are multiple data
+    serializer = PeopleSerializers(objs, many=True)
+    return Response(serializer.data)
+  
+  elif request.method == "POST":
+    data = request.data
+    # convert the JSON to QuerySet
+    serializer = PeopleSerializers(data=data)
+    # data validation
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    # Error if the data is not valid
+    return Response(serializer.errors)
+  
+  # does full update
+  elif request.method == "PUT":
+    data = request.data
+    # convert the JSON to QuerySet
+    serializer = PeopleSerializers(data=data)
+    # data validation
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    # Error if the data is not valid
+    return Response(serializer.errors)
+  
+  # supports partial update. only send fields you want to update here
+  elif request.method == "PATCH":
+    data = request.data
+
+    obj = Person.objects.get(id=data['id'])
+    # convert the JSON to QuerySet. data bef, field we want to update
+    serializer = PeopleSerializers(obj, data=data, partial=True)
+    # data validation
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    # Error if the data is not valid
+    return Response(serializer.errors)
+  
+  # Delete data
+  # throws exception in data not found or error
+  else:
+    data = request.data
+    obj = Person.objects.get(id=data["id"])
+    obj.delete()
+    return Response({"message":"person deleted"})
