099625b02f744ac9e106457902e174b2feb96f23
ShafiArnob <shafiarnob@gmail.com>
2023-06-13 09:59:37 +0600

    http methods

--- drf-getting-started/core/home/views.py
+++ drf-getting-started/core/home/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(["GET", "POST", "PUT"]) # for http methods
+def index(request):
+  courses = {
+    "name":"Arnob",
+    "learns":["django", "AI"],
+    "course":"AI"
+  }
+
+  if request.method == 'GET': #check HTTP Method
+    print("GET Method")
+    return Response(courses)
+  
+  elif request.method == 'POST':
+    print("POST Method")
+    return Response(courses)
+  elif request.method == 'PUT':
+    print("PUT Method")
+    return Response(courses)
