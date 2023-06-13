2d5e87f47d0bc74220c532c6e4fe1b2f4d3e6864
ShafiArnob <shafiarnob@gmail.com>
2023-06-13 10:25:19 +0600

    serializers

---
+++ drf-getting-started/core/home/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Person
+
+class PeopleSerializers(serializers.ModelSerializer):
+  class Meta:
+    model = Person
+    # fields you want to serialize
+    # if specific fields -> ["name", "age"]
+    # if you want to add all the fields excluding some fields 
+    # then exclude = ["age"]
+    fields = '__all__'
