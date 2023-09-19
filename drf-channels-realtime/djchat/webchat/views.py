c8783fd3cac2081badf237af6ff99517b35f9b3c
ShafiArnob <shafiarnob@gmail.com>
2023-09-19 22:57:44 +0600

    uni-management auth service init

--- drf-channels-realtime/djchat/webchat/views.py
+++ drf-channels-realtime/djchat/webchat/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.routers import Response
+from .serializer import MessageSerializer
+from .models import Conversation
 
-# Create your views here.
+class MessageViewSet(viewsets.ViewSet):
+  def list(self, request):
+    channel_id = request.query_params.get("channel_id")
+
+    try:
+      conversation = Conversation.objects.get(channel_id=channel_id)
+      messages = conversation.message.all()
+
+      serializer = MessageSerializer(messages, many=True)
+      return Response(serializer.data)
+    except Conversation.DoesNotExist:
+      return Response([])
