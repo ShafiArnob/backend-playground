a0ffea39b21cbacd20b684b6a6484b60ec1afbc6
ShafiArnob <shafiarnob@gmail.com>
2023-10-21 02:00:01 +0600

    custom log

--- university-management-auth-service/src/shared/logger.ts
+++ university-management-auth-service/src/shared/logger.ts
@@ -1,32 +1,43 @@
 /* eslint-disable no-undef */
-import path from 'path';
-import winston from "winston";
+import path from 'path'
+import { createLogger, format, transports } from 'winston'
+const { combine, timestamp, label, printf } = format
 
-const logger = winston.createLogger({
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+
+  return `${date.toDateString()} ${hour}:${minutes}:${seconds} [${label}] ${level}: ${message}`
+})
+
+const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(label({ label: 'right meow!' }), timestamp(), myFormat),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      filename: path.join(process.cwd(), "logs", "winston", "success.log"), 
-      level: 'info' })
+    new transports.Console(),
+    new transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
+      level: 'info',
+    }),
   ],
-});
+})
 
 //only for logging errors
-const errorlogger = winston.createLogger({
+const errorlogger = createLogger({
   level: 'error',
-  format: winston.format.json(),
+  format: combine(label({ label: 'right meow!' }), timestamp(), myFormat),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ 
-      filename: path.join(process.cwd(), "logs", "winston", "error.log"),
-      level: 'error' 
-    })
+    new transports.Console(),
+    new transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
+      level: 'error',
+    }),
   ],
-});
+})
 
-export { errorlogger, logger };
+export { errorlogger, logger }
 
