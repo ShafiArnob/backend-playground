fd7bbdef0ab4f5572af2f31fa5e98ad363f93cae
ShafiArnob <shafiarnob@gmail.com>
2024-01-24 19:26:58 +0600

    zod basics

--- 100x-node-express/index.js
+++ 100x-node-express/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const zod = require("zod");
 const app = express();
 const PORT = 4000;
+
+const userSchema = zod.object({
+  email: zod.string().email(),
+  password: zod.string().min(8),
+  country: zod.literal("IN").or(zod.literal("US")),
+});
 
 let numOfRequests = 0;
 const calculateReqs = (req, res, next) => {
@@ -9,8 +16,24 @@ const calculateReqs = (req, res, next) => {
   next();
 };
 
-app.get("/hit", calculateReqs, (req, res) => {
+app.use(calculateReqs);
+app.use(express.json());
+
+app.get("/hit", (req, res) => {
   console.log("Hit");
+  res.send(`Requests ${numOfRequests}`);
+});
+
+app.post("/user", (req, res) => {
+  const userData = req.body;
+  const response = userSchema.safeParse(userData);
+  // console.log(req.body.kid);
+  res.json({ response });
+});
+
+// global error catch
+app.use(function (err, req, res, next) {
+  res.json({ message: "Error in the server" });
 });
 
 app.listen(PORT, () => {
