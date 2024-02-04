3ed591a8bf89963113d6de8842b5330520421e8a
ShafiArnob <shafiarnob@gmail.com>
2024-02-04 11:36:23 +0600

    prisma-node: create nested data

---
+++ prisma-nodejs-basics/src/create-user-posts.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: "arnob@gmail.com",
+      name: "Arnob",
+      posts: {
+        create: [{ title: "Arnob 1" }, { title: "Arnob 2" }],
+      },
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
