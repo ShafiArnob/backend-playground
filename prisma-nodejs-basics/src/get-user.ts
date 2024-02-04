f624f28c3dea8c373f4e6df62868d7a5fdd61faa
ShafiArnob <shafiarnob@gmail.com>
2024-02-04 12:01:22 +0600

    prisma-node: get all & nested data

---
+++ prisma-nodejs-basics/src/get-user.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  //get all the "users"
+  const users = await prisma.user.findMany({});
+  console.log(users);
+
+  //get single "users" with the users "posts"
+  const user = await prisma.user.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      posts: true,
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .then(async () => {
+    console.log("done");
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
