ab5d6e492453e5f2997a7133cff732891f5097c5
ShafiArnob <shafiarnob@gmail.com>
2024-02-04 11:29:07 +0600

    prisma-node: create realtional data

---
+++ prisma-nodejs-basics/src/create-post.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({ log: ["info", "query"] });
+
+async function main() {
+  await prisma.post.create({
+    data: {
+      title: "title of post",
+      content: "lorem 100",
+      author: {
+        connect: {
+          id: 1,
+        },
+      },
+      // or
+      // authorId: 1
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
