25326b5a8ce609e06f34422455fb269955631a52
ShafiArnob <shafi.arnob@inceptasolutions.com>
2024-03-26 13:17:56 +0600

    remove django projects

--- drf-channels-realtime/djchat/server/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Category, Channel, Server
-
-# Register your models here.
-admin.site.register(Channel)
-admin.site.register(Server)
-admin.site.register(Category)
