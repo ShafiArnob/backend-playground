6dd6c9c3345d73e2e0034620c3dda788f83a4636
ShafiArnob <shafiarnob@gmail.com>
2024-03-30 08:32:08 +0600

    update user profile - image upload

--- advance-news-api-express/controller/ProfileController.js
+++ advance-news-api-express/controller/ProfileController.js
@@ -1,3 +1,6 @@
+import { generateRandomNum, imageValidator } from "../utils/helper.js";
+import prisma from "../DB/db.config.js";
+
 class ProfileController {
   static async index(req, res) {
     try {
@@ -11,7 +14,57 @@ class ProfileController {
   static async show(req, res) {}
   static async update(req, res) {
     try {
-    } catch (e) {}
+      const { id } = req.params;
+      console.log("Profile Id", id);
+      //check if there is files
+      if (!req.files || Object.keys(req.files).length === 0) {
+        return res
+          .status(400)
+          .json({ status: 400, message: "Profile image is required." });
+      }
+
+      //check file is right
+      const profile = req.files.profile;
+      const message = imageValidator(profile?.size, profile.mimetype);
+      if (message !== null) {
+        return res.status(400).json({
+          errors: {
+            profile: message,
+          },
+        });
+      }
+
+      // processing image name
+      const imgExt = profile?.name.split(".");
+      const newImageName = generateRandomNum() + "." + imgExt[1];
+      const uploadPath = process.cwd() + "/public/images" + newImageName;
+
+      // if error uploading photo
+      profile.mv(uploadPath, (err) => {
+        if (err) throw err;
+      });
+
+      // update path of image to DB
+      await prisma.users.update({
+        data: {
+          profile: newImageName,
+        },
+        where: {
+          id: Number(id),
+        },
+      });
+
+      return res.status(200).json({
+        status: "success",
+        message: "Profile updated successfully!",
+      });
+    } catch (e) {
+      console.log(e);
+      return res.status(500).json({
+        status: "failed",
+        message: "Something went wrong please try again",
+      });
+    }
   }
   static async destroy(req, res) {}
 }
--- advance-news-api-express/utils/helper.js
+++ advance-news-api-express/utils/helper.js
@@ -1,3 +1,6 @@
+import { supportedMimes } from "../config/filesystem.js";
+import { v4 as uuidv4 } from "uuid";
+
 export const imageValidator = (size, mime) => {
   if (bytesToMb(size) > 2) {
     return "Image size must be less than 2 MB";
@@ -10,4 +13,8 @@ export const imageValidator = (size, mime) => {
 
 export const bytesToMb = (bytes) => {
   return bytes / (1024 * 1024);
+};
+
+export const generateRandomNum = () => {
+  return uuidv4();
 };
