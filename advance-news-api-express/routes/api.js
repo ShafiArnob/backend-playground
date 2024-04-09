73cf0574c4f60371f0f4735b7ec4053571958744
your name <shafiarnob@gmail.com>
2024-04-09 15:46:09 +0600

    cache all news using redis

--- advance-news-api-express/routes/api.js
+++ advance-news-api-express/routes/api.js
@@ -3,6 +3,7 @@ import AuthController from "../controller/AuthController.js";
 import ProfileController from "../controller/ProfileController.js";
 import authMiddleware from "../middleware/Authenticate.js";
 import NewsController from "../controller/NewsController.js";
+import redisCache from "../DB/redis.config.js";
 
 const router = Router();
 
@@ -15,7 +16,7 @@ router.get("/profile", authMiddleware, ProfileController.index);
 router.put("/profile/:id", authMiddleware, ProfileController.update);
 
 //& News routes
-router.get("/news", NewsController.index);
+router.get("/news", redisCache.route(), NewsController.index);
 router.post("/news", authMiddleware, NewsController.store);
 router.get("/news/:id", authMiddleware, NewsController.show);
 router.patch("/news/:id", authMiddleware, NewsController.update);
