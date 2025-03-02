892d504fb9260ad913bbaed8fa1f26d07e5b7afd
your name <shafiarnob@gmail.com>
2025-03-02 14:00:48 +0600

    fastapi: connections, lifespan events, models

--- fastapi-beyond-crud/src/__init__.py
+++ fastapi-beyond-crud/src/__init__.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
 from src.books.routes import book_router
+from src.db.main import init_db
+
+
+# determine when your app is going to start and when to end
+@asynccontextmanager
+async def life_span(app):
+    print(f"Server is starting ...")
+    await init_db()
+    yield
+    print(f"Server is stopped")
+
 
 version = "v1"
 
@@ -7,6 +20,7 @@ app = FastAPI(
     title="Bookly",
     description="A REST API for a book review web service",
     version=version,
+    lifespan=life_span,
 )
 
 app.include_router(book_router, prefix=f"/api/{version}/books")
