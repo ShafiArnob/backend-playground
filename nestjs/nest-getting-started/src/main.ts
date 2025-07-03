fa7240ad505a56dce7830786f73a3b28fe40da0b
your name <shafiarnob@gmail.com>
2025-07-03 20:38:16 +0600

    nest: dtos, pipes and validators

--- nestjs/nest-getting-started/src/main.ts
+++ nestjs/nest-getting-started/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Tell the application to use validation pipes. Declared globally
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips out elements that is not defined in dtos.
+    }),
+  );
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
